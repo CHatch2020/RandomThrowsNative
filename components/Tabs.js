2188a384beb8f432730f07cad86e20184241a2dc
Caleb Hatch <hatchcaleb@gmail.com>
2023-11-26 12:19:45 -0600

    Tabs Navigation ready to be used once Pages are done

---
+++ components/Tabs.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// const Tab = createBottomTabNavigator();
+
+// const Tabs = {()} => {
+//     return (
+//         <Tab.Navigator>
+//             <Tab.Screen>
+
+//             </Tab.Screen>
+//         </Tab.Navigator>
+//     )
+// }
+
+// export default Tabs
