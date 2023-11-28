e76dca00b5c5da1c2538fb1e5d96c74230105fcf
Caleb Hatch <hatchcaleb@gmail.com>
2023-11-27 19:51:40 -0600

    changed home page to jsx; added theme file

---
+++ theme.js
@@ -0,0 +1,62 @@
+// theme.js
+
+const Colors = {
+    primary: '#3498db',
+    secondary: '#2ecc71',
+    accent: '#e74c3c',
+    background: '#ecf0f1',
+    text: '#2c3e50',
+    placeholderText: '#bdc3c7',
+    border: '#dcdde1',
+    success: '#27ae60',
+    warning: '#f39c12',
+    danger: '#e74c3c',
+    info: '#3498db',
+    light: '#f5f6fa',
+    dark: '#2c3e50',
+    transparent: 'transparent',
+  };
+  
+  const Fonts = {
+    regular: 'Roboto-Regular',
+    bold: 'Roboto-Bold',
+    medium: 'Roboto-Medium',
+    light: 'Roboto-Light',
+    // Add more fonts as needed
+  };
+  
+  const FontSizes = {
+    small: 12,
+    regular: 14,
+    medium: 16,
+    large: 18,
+    extraLarge: 24,
+    // Add more font sizes as needed
+  };
+  
+  const Spacing = {
+    small: 8,
+    medium: 16,
+    large: 24,
+    extraLarge: 32,
+    // Add more spacing values as needed
+  };
+  
+  const BorderRadius = {
+    small: 4,
+    medium: 8,
+    large: 12,
+    // Add more border radius values as needed
+  };
+  
+  const Theme = {
+    colors: Colors,
+    fonts: Fonts,
+    fontSizes: FontSizes,
+    spacing: Spacing,
+    borderRadius: BorderRadius,
+    // Add more theme properties as needed
+  };
+  
+  export default Theme;
+  
